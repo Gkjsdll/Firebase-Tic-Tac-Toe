df17dfe565522354442417e5785b73715fc4ce81
Zack Winchell <zackwinchell@gmail.com>
2016-01-20 09:35:19 -0800

    players are now tracked correctly accross boards

--- main.js
+++ main.js
@@ -26,7 +26,6 @@ function nameAvailable(name){
         isAvailable = false;
       }
     })
-    console.log("Username available: ", isAvailable);
     if(isAvailable){
       checkOnline(name);
       gameOver = false;
@@ -41,7 +40,7 @@ function nameAvailable(name){
 $(document).ready(function(){
 
   var $squares = $('.square');
-  $("#newGame").click(clearBoard)
+  $("#newGame").click(newBoard)
 
 
   $squares.click(squareClickHandler);
@@ -61,7 +60,7 @@ function squareClickHandler(){ //must be adapted for firebase
       var square = $(this).attr('id')
       var boardSquare = {};
       boardSquare[square] = whoseTurn;
-      mainRef.child("boards").child(currentBoard).update(boardSquare);
+      mainRef.child("boards").child(currentBoard+"/squares/").update(boardSquare);
       $this.css("cursor","default");
       switch(whoseTurn){
         case "X":
@@ -71,23 +70,11 @@ function squareClickHandler(){ //must be adapted for firebase
         whoseTurn = "X";
         break;
       }
+
       // checkWin($this);
     }
   }
 };
-
-
-
-function clearBoard(){
-  mainRef.child("boards").child(currentBoard).once("value", function(squares){
-    squares.forEach(function(square){
-      var key = square.key();
-      var boardSquare = {};
-      boardSquare[key] = "Empty";
-      mainRef.child("boards").child(currentBoard).update(boardSquare);
-    });
-  });
-}
 
 function initUsername(){
   if(!localStorage.username){
@@ -103,28 +90,30 @@ function initUsername(){
   }
   else {
     checkOnline(localStorage.username);
-    currentBoard = localStorage.currentBoard;
     gameOver = false;
-    getBoard();
+    currentBoard = localStorage.currentBoard;
   }
 }
 
-function getBoard(){ //also remove listener for previous board if there is one
-  debugger;
+function getBoard(){
   mainRef.once("value", function(snap){
     if(!snap.child("boards").exists()){
       newBoard();
+      //   mainRef.child("boards").limitToLast(1).once("value", function(boardSnap){
+      //     mainRef.child("users").child(localStorage.username).child("board").once("value", function(snap){
+      //       debugger;
+      //   });
+      // });
     }
 
     if(!snap.child("users").child(localStorage.username).child("board").exists()){
       mainRef.child("boards").limitToLast(1).once("value", function(snap){
         for (var key in snap.val()){
           var boardRef = new Firebase(mainRef.child("boards").limitToLast(1).toString() + "/" + key);
-          debugger;
           boardRef.child("users").once("value", function(snap){
             var users = 0;
             for(var i in snap.val()){
-              users++;
+              if(i !== localStorage.username) users++;
             }
             if(users < 2){
               var thisUser = {};
@@ -141,22 +130,24 @@ function getBoard(){ //also remove listener for previous board if there is one
             }
             else{
               newBoard();
-              getBoard();
             }
-            debugger;
           });
         }
       })
       //find last board;
     }
     else{
-      // currentBoard = boardKeyFromFirebase
+      mainRef.child("users").child(localStorage.username).once("value", function(snap){
+        currentBoard = snap.val()["board"];
+        localStorage.currentBoard = currentBoard;
+        watchBoard();
+      });
     }
   });
 }
 
 function watchBoard(){
-  mainRef.child("boards").child(currentBoard).on("value", function(squares){
+  mainRef.child("boards").child(currentBoard+"/squares/").on("value", function(squares){
     squares.forEach(function(square){
       var whichSquare = $('#'+square.key())
       switch(square.val()){
@@ -188,4 +179,22 @@ function newBoard(){
   square8: "Empty",
   square9: "Empty"};
   mainRef.child("boards").push({"squares": board});
+
+  mainRef.child("users").child(localStorage.username).once("value", function(snap){
+    if(currentBoard){
+      var userGone = {};
+      userGone[localStorage.username] =  "Gone";
+      mainRef.child("boards/"+currentBoard+"/users").update(userGone);
+    }
+    mainRef.child("boards").limitToLast(1).once("value", function(snap){
+      for (var key in snap.val()){
+        mainRef.child("users").child(localStorage.username).update({"board": key});
+        var playerX = {};
+        playerX[localStorage.username] = "X";
+        mainRef.child("boards/"+key).child("users").update(playerX);
+        currentBoard = key;
+        watchBoard();
+      }
+    });
+  });
 }
