7d408fd48858b516807b73b3ee9c7e8752265689
Zack Winchell <zackwinchell@gmail.com>
2016-01-20 11:12:01 -0800

    board listener now updates properly if joining existing board with newGame

--- main.js
+++ main.js
@@ -56,7 +56,6 @@ function squareClickHandler(){ //must be adapted for firebase
   mainRef.child("boards/"+currentBoard).once("value", function(snap){
     if(!gameOver && snap.val()["whoseTurn"] === localStorage.player){
       if($this.text() === ""){
-        debugger;
         var square = $this.attr('id')
         var boardSquare = {};
         boardSquare[square] = localStorage.player;
@@ -175,6 +174,7 @@ function newBoard(){
 
 function newGame(){
   assignBoard();
+  gameOver = false;
 }
 
 function assignBoard(){
@@ -202,6 +202,7 @@ function assignBoard(){
           mainRef.child("boards").child(currentBoard+"/squares/").off();
           boardRef.child("users").update(thisUser);
           mainRef.child("users").child(localStorage.username).update({"board": key});
+          currentBoard = key;
           watchBoard();
         }
         else{
@@ -210,4 +211,59 @@ function assignBoard(){
       });
     }
   })
+}
+
+
+function checkWin($square){
+  spacesRemaining--;
+  switch($square.index()){
+    case 0:
+      checkSquares(1,2);
+      checkSquares(3,6);
+      checkSquares(4,8);
+      break;
+    case 1:
+      checkSquares(0,2);
+      checkSquares(4,7);
+      break;
+    case 2:
+      checkSquares(0,1);
+      checkSquares(4,6);
+      checkSquares(5,8);
+      break;
+    case 3:
+      checkSquares(0,6);
+      checkSquares(4,5);
+      break;
+    case 4:
+      for(var i = 0; i < 4; i++){
+        checkSquares(i, 8-i);
+      }
+      break;
+    case 5:
+      checkSquares(2,8);
+      checkSquares(3,4);
+      break;
+    case 6:
+      checkSquares(0,3);
+      checkSquares(2,4);
+      checkSquares(7,8);
+      break;
+    case 7:
+      checkSquares(1,4);
+      checkSquares(6,8);
+      break;
+    case 8:
+      checkSquares(0,4);
+      checkSquares(2,5);
+      checkSquares(6,7);
+      break;
+  }
+  if(!spacesRemaining && !gameOver){
+    noWin();
+  }
+};
+
+function noWin(){
+  gameOver = true;
 }
